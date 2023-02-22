47133cac4664fdd1187c7acd54c2609818012481
KhaledxMohamed1955 <khaledmnour1955@outlook.com>
2023-02-22 13:01:34 +0200

    quik fix new programmer sorry

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,15 @@
  *
  * Return: Will always return 0
  */
-int main(void){
-	char[8] var = "_putchar";
-	printf("%c\n", var);
-	return 0
+int main(void)
+{
+	char var[8] = "_putchar";
+	int i;
+
+	for (i=0; i< 8; i++)
+	{
+		putchar(var[i]);
+	}
+	putchar('\n');
+	return 0;
 }
