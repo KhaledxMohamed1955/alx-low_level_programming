686e5b78300c634aa53f3c5dab5443316336c2aa
KhaledxMohamed1955 <khaledmnour1955@outlook.com>
2023-02-23 08:28:11 +0200

    1-main debugg

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - check if infinite or not.
+ * Return: Will return 0 if true and 1 if false.
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
