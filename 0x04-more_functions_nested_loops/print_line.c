6477c20f7755123286799a391f6eab96b82dcac0
KhaledxMohamed1955 <khaledmnour1955@outlook.com>
2023-02-23 07:38:15 +0200

    print line for n times

---
+++ 0x04-more_functions_nested_loops/print_line.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_line - print one line n of time
+ * @n: intger.
+ * Return: line n of times.
+ */
+void print_line(void)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
