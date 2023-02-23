ca5e2d2b97f36dbb171ca7f1ddde176b54380478
KhaledxMohamed1955 <khaledmnour1955@outlook.com>
2023-02-23 08:24:50 +0200

    first Debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test the function if works
+ * Return: Will return 0 if ture.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
