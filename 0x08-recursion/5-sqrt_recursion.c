1eb2969634a887fa7e7df8154ba89309cedd80f9
KhaledxMohamed1955 <khaledmnour1955@outlook.com>
2023-03-08 11:08:04 +0200

    square root in c recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * power_operation - squre root.
+ * @n: input integer.
+ * @c: iterator.
+ * Return: square root.
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - prints the natural square root.
+ * @n: input integer.
+ * Return: natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
+
